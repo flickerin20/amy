2bf2e0316c2573a3eb793b62752fb03505508a65
Vinod Kumar <vinodkumar.selvaraj@flickerin.co.in>
2020-05-30 12:55:10 +0530

    MAT-3 SUBBU COMMIT vinod revisit

--- app/src/main/java/com/flickerin/amy/MainActivity.java
+++ app/src/main/java/com/flickerin/amy/MainActivity.java
@@ -23,6 +23,5 @@ public class MainActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
-        //finish();
     }
 }
